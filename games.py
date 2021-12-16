5623b3bd3522e80209133487e492988d2aef2a21
jcksndy <jcksnmdy1@gmail.com>
2021-12-16 02:24:13 -0500

    hh

--- games.py
+++ games.py
@@ -95,7 +95,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
  
-#client.connect(MQTT_SERVER, 1883, 60)
+client.connect(MQTT_SERVER, 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
